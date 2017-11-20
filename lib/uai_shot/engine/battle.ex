ae8668599abdb9f6554308ae49c66d9d7edf0f94
Sérgio Rodrigues <sergioaugrod@gmail.com>
2017-11-20 17:06:39 -0200

    Creates module to encapsule battle logic]

---
+++ lib/uai_shot/engine/battle.ex
@@ -0,0 +1,69 @@
+defmodule UaiShot.Engine.Battle do
+  @moduledoc """
+  Game battle logic.
+  """
+
+  alias UaiShot.Store.{Bullet, Player, Ranking}
+  alias UaiShotWeb.Endpoint
+
+  @game_width 800
+  @game_height 600
+
+  @doc """
+  Run game battle logic.
+  """
+  @spec run() :: :ok
+  def run do
+    bullets = Bullet.all()
+    |> Enum.map(&move_bullet(&1))
+    |> Enum.reject(&bullet_is_far?(&1))
+
+    Bullet.reset(bullets)
+    Endpoint.broadcast("game:lobby", "update_bullets", %{bullets: bullets})
+  end
+
+  @spec move_bullet(Map.t) :: Map.t
+  defp move_bullet(bullet) do
+    bullet
+    |> hited_players
+    |> Enum.each(&process_hit(bullet, &1))
+
+    bullet
+    |> Map.put(:x, bullet.x + bullet.speed_x)
+    |> Map.put(:y, bullet.y + bullet.speed_y)
+  end
+
+  @spec process_hit(Map.t, Map.t) :: :ok
+  defp process_hit(bullet, player) do
+    update_ranking(bullet.player_id, 10)
+    update_ranking(player.id, -10)
+
+    Endpoint.broadcast("game:lobby", "update_ranking", %{ranking: Ranking.all()})
+    Endpoint.broadcast("game:lobby", "hit_player", %{player_id: player.id})
+  end
+
+  @spec update_ranking(String.t, Integer.t) :: :ok
+  defp update_ranking(player_id, value) do
+    player_id
+    |> Ranking.get
+    |> Map.update!(:value, &(&1 + value))
+    |> Ranking.put
+  end
+
+  @spec bullet_is_far?(Map.t) :: Boolean.t
+  defp bullet_is_far?(bullet) do
+    bullet.x < -10 || bullet.x > @game_width
+    || bullet.y < -10 || bullet.y > @game_height
+  end
+
+  @spec hited_players(Map.t) :: Enum.t
+  defp hited_players(bullet) do
+    Player.all()
+    |> Enum.filter(&(bullet.player_id != &1.id))
+    |> Enum.filter(fn player ->
+      dx = player.x - bullet.x
+      dy = player.y - bullet.y
+      :math.sqrt(dx * dx + dy * dy) < 30
+    end)
+  end
+end
--- lib/uai_shot/game_server.ex
+++ lib/uai_shot/game_server.ex
@@ -1,17 +1,13 @@
 defmodule UaiShot.GameServer do
   @moduledoc """
-  Update game data such as bullet positions and if the player
-  has been hit by a bullet.
+  GameServer loop. Executes all game engines.
   """
 
   use GenServer
 
-  alias UaiShot.Store.{Bullet, Player, Ranking}
-  alias UaiShotWeb.Endpoint
+  alias UaiShot.Engine.Battle
 
   @worker_interval 20
-  @game_width 800
-  @game_height 600
 
   @doc """
   Start GameServer.
@@ -31,62 +27,11 @@ defmodule UaiShot.GameServer do
   end
 
   @doc """
-  Update bullet positions and hit players.
+  Executes all game engines.
   """
   @spec handle_info(:work, State.t) :: {:noreply, State.t}
   def handle_info(:work, state) do
-    bullets = Bullet.all()
-    |> Enum.map(&move_bullet(&1))
-    |> Enum.reject(&bullet_is_far?(&1))
-
-    Bullet.reset(bullets)
-    Endpoint.broadcast("game:lobby", "update_bullets", %{bullets: bullets})
-
+    Battle.run()
     {:noreply, state}
-  end
-
-  @spec move_bullet(Map.t) :: Map.t
-  defp move_bullet(bullet) do
-    bullet
-    |> hited_players
-    |> Enum.each(&process_hit(bullet, &1))
-
-    bullet
-    |> Map.put(:x, bullet.x + bullet.speed_x)
-    |> Map.put(:y, bullet.y + bullet.speed_y)
-  end
-
-  @spec process_hit(Map.t, Map.t) :: :ok
-  defp process_hit(bullet, player) do
-    update_ranking(bullet.player_id, 10)
-    update_ranking(player.id, -10)
-
-    Endpoint.broadcast("game:lobby", "update_ranking", %{ranking: Ranking.all()})
-    Endpoint.broadcast("game:lobby", "hit_player", %{player_id: player.id})
-  end
-
-  @spec update_ranking(String.t, Integer.t) :: :ok
-  defp update_ranking(player_id, value) do
-    player_id
-    |> Ranking.get
-    |> Map.update!(:value, &(&1 + value))
-    |> Ranking.put
-  end
-
-  @spec bullet_is_far?(Map.t) :: Boolean.t
-  defp bullet_is_far?(bullet) do
-    bullet.x < -10 || bullet.x > @game_width
-    || bullet.y < -10 || bullet.y > @game_height
-  end
-
-  @spec hited_players(Map.t) :: Enum.t
-  defp hited_players(bullet) do
-    Player.all()
-    |> Enum.filter(&(bullet.player_id != &1.id))
-    |> Enum.filter(fn player ->
-      dx = player.x - bullet.x
-      dy = player.y - bullet.y
-      :math.sqrt(dx * dx + dy * dy) < 30
-    end)
   end
 end
