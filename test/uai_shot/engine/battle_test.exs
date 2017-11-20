87f0d654f369864584a590c488e4af461e29bcb6
Sérgio Rodrigues <sergioaugrod@gmail.com>
2017-11-20 17:54:46 -0200

    Adds battle test

---
+++ test/uai_shot/engine/battle_test.exs
@@ -0,0 +1,56 @@
+defmodule UaiShot.Engine.BattleTest do
+  use UaiShotWeb.ChannelCase, async: true
+
+  alias UaiShot.Engine.Battle
+  alias UaiShotWeb.GameChannel
+  alias UaiShot.Store.{Bullet, Player, Ranking}
+
+  setup_all do
+    Bullet.start_link()
+    Player.start_link()
+    Ranking.start_link()
+    :ok
+  end
+
+  setup do
+    Bullet.reset([])
+
+    {:ok, _, socket} = socket("player_id", %{player_id: 1})
+    |> subscribe_and_join(GameChannel, "game:lobby")
+
+    [socket: socket]
+  end
+
+  describe "run/0" do
+    test "broadcast empty bullets" do
+      Battle.run()
+      assert_broadcast "update_bullets", %{bullets: []}
+    end
+
+    test "broadcast bullets" do
+      bullet = %{player_id: 1, x: 1, y: 2, rotation: 1, speed_x: 1, speed_y: 2}
+      Bullet.push(bullet)
+
+      Battle.run()
+      assert_broadcast "update_bullets",
+        %{bullets: [%{player_id: 1, x: 2, y: 4, rotation: 1, speed_x: 1, speed_y: 2}]}
+    end
+
+    test "hit player and update ranking" do
+      player = %{id: 1, nickname: "John", x: 1, y: 2, rotation: 1}
+      player2 = %{id: 2, nickname: "Bruce", x: 1, y: 10, rotation: 1}
+
+      bullet = %{player_id: 1, x: 1, y: 10, rotation: 1, speed_x: 1, speed_y: 2}
+
+      Bullet.push(bullet)
+      Player.put(player)
+      Player.put(player2)
+
+      assert_broadcast "update_ranking",
+        %{ranking: [%{nickname: "John", player_id: 1, value: 10},
+                    %{nickname: "Bruce", player_id: 2, value: -10}]}
+
+      assert_broadcast "hit_player", %{player_id: 2}
+    end
+  end
+end
