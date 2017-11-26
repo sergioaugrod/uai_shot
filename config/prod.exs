2d1f89c9ef9808b6575704e3b1f7081e758454dd
Sérgio Rodrigues <sergioaugrod@gmail.com>
2017-11-26 19:48:09 -0200

    Changes check_origin to false

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :uai_shot, UaiShotWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "localhost", port: 80],
+  check_origin: false,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
