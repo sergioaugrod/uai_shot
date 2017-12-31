1c2bab8314a0175f4f046b6ea93d614494f79265
Sérgio Rodrigues <sergioaugrod@gmail.com>
2017-12-31 01:40:12 -0200

    Adds player name and different sprite for enemies

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,7 +23,6 @@ import {Game} from "./game";
 let nickname = prompt("Hello! What's your name?");
 let engine = new Phaser.Game(800, 600, Phaser.CANVAS, "phaser", { preload: preload, create: create, update: update });
 let game = new Game(engine, nickname);
-game.start();
 
 function preload() {
     game.preload(this);
--- assets/js/game.js
+++ assets/js/game.js
@@ -8,20 +8,24 @@ export class Game {
         this.playerId = null;
         this.nickname = nickname;
         this.engine = engine;
+        this.ready = false;
     }
 
     start() {
-        this._connectToLobby();
         this._updateRanking();
         this._hitPlayer();
         this._updateBullets();
         this._updatePlayers();
+        this.ready = true;
     }
 
     preload(state) {
         this.engine.load.image("bullet", "images/bullet.png");
         this.engine.load.image("ship", "images/ship.png");
+        this.engine.load.image("ship-enemy", "images/ship-enemy.png");
         this.engine.load.image("space", "images/space.png");
+        this.engine.load.image("trophy", "images/trophy.png");
+        this._connectToLobby();
     }
 
     create(state) {
@@ -29,15 +33,19 @@ export class Game {
         this._setTexts();
         this._setKeyboard(state);
         this._createPlayer();
+        this.start();
     }
 
     update(state) {
-        this.player.update(this.engine, this.cursors, this.shootButton, this.channel);
-        this._updateAlpha();
+        if(this.ready) {
+            this.player.update(this.engine, this.cursors, this.shootButton, this.channel);
+            this._updateAlpha();
+        }
     }
 
     _setTexts() {
-        this.ranking = this.engine.add.text(10, 10, "", { font: "14px Arial", fill: "#fff" });
+        this.ranking = this.engine.add.text(38, 10, "", { font: "14px Arial", fill: "#fff" });
+        this.engine.add.sprite(10, 6, "trophy");
         this.playersOnline = this.engine.add.text(10, 580, "", { font: "14px Arial", fill: "#F4D03F" });
     }
 
@@ -65,6 +73,10 @@ export class Game {
         this.channel.push("new_player", { x: sprite.x, y: sprite.y, rotation: sprite.rotation });
     }
 
+    _createNicknameText(x, y, nickname) {
+        return this.engine.add.text(x, y, nickname, { font: "10px Arial", fill: "#F4D03F" });
+    }
+
     _createShip(x, y, type, rotation) {
         let sprite = this.engine.add.sprite(x, y, type);
         this.engine.physics.arcade.enable(sprite);
@@ -80,19 +92,25 @@ export class Game {
         this.channel.on("update_players", payload => {
             payload.players.forEach(player => {
                 if(!this.players[player.id] && player.id != this.playerId) {
-                    this.players[player.id] = this._createShip(player.x, player.y, "ship", player.rotation);
+                    this.players[player.id] = this._createShip(player.x, player.y, "ship-enemy", player.rotation);
+                    this.players[player.id].text = this._createNicknameText(player.x, player.y-25, player.nickname.substring(0, 8));
                 }
 
                 if (player.id != this.playerId) {
                     this.players[player.id].x = player.x;
                     this.players[player.id].y = player.y;
                     this.players[player.id].rotation = player.rotation;
+
+                    this.players[player.id].text.x = player.x;
+                    this.players[player.id].text.y = player.y+25;
+                    this.players[player.id].text.rotation = player.rotation;
                 }
             });
 
             for(let id in this.players) {
                 if(!payload.players.some(p => p.id == id)) {
                     this.players[id].destroy();
+                    this.players[id].text.destroy();
                     delete this.players[id];
                 }
             }
@@ -134,9 +152,9 @@ export class Game {
             let rankingText = payload.ranking
                 .map(position => {
                     if(position.player_id == this.playerId) {
-                        return `${position.nickname}: ${position.value} (me)`;
+                        return `${position.nickname.substring(0, 8)}: ${position.value} (me)`;
                     } else {
-                        return `${position.nickname}: ${position.value}`;
+                        return `${position.nickname.substring(0, 8)}: ${position.value}`;
                     }
                 }).join("\n");
 
