c34c9fe89cd689123f8750b15b7a5e7f8e918c9b
Sérgio Rodrigues <sergioaugrod@gmail.com>
2019-08-06 22:55:55 -0300

    Fix port in the config/releases.exs

--- config/releases.exs
+++ config/releases.exs
@@ -2,6 +2,5 @@ import Config
 
 config :uai_shot, UaiShotWeb.Endpoint,
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
-  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "80")],
   secret_key_base: System.get_env("SECRET_KEY_BASE")
-
